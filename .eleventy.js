bd13419c2404d75aa814926c7ebf950ba05da9c5
marcelocho <marcelo.cho@gmail.com>
2025-10-21 12:23:09 -0300

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/pdf");
+  
+  
+  eleventyConfig.setServerOptions({
+    middleware: [
+        function (req, res, next) {
+            if (req.statusCode === 404) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(require("fs").readFileSync("_site/404.html"));
+            } else {
+                next();
+            }
+        }
+    ]
+});
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
+
